017bd74f68a57e68dc49c5b82bccef61fa793e03
Laila Aquino <lailaaquino8@gmail.com>
2026-05-30 14:32:53 -0300

    Initial commit

---
+++ order/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/lailaaquino/microservices/order/config"
+	"github.com/lailaaquino/microservices/order/internal/adapters/db"
+
+	//"github.com/lailaaquino/microservices/order/internal/adapters/rest"
+	"github.com/lailaaquino/microservices/order/internal/adapters/grpc"
+
+	"github.com/lailaaquino/microservices/order/internal/application/core/api"
+)
+
+func main() {
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
